68229a6b2d83acfc374a3c69f26db1a3678fe426
Leticia Rocha <l.rocha.gonzalez@mobgen.com>
2019-11-12 15:26:30 +0100

    login activity

--- app/src/main/java/com/pickency/karumi/MainActivity.kt
+++ app/src/main/java/com/pickency/karumi/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.pickency.karumi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loginButton.setOnClickListener {
+            if (usernameEditText.text.toString().equals("galicia") && passwordEditText.text.toString().equals("llueve")) {
+                loginSuccess()
+            } else
+                showError("Error en el login")
+        }
+        logoutButton.setOnClickListener {
+            if (System.currentTimeMillis() / 1000 % 2 == 0L) {
+                logoutSuccess()
+            } else
+                showError("Error en el logout")
+        }
+
     }
+
+    private fun loginSuccess() {
+        logoutButton.visibility = View.VISIBLE
+        group.visibility = View.GONE
+    }
+
+    private fun logoutSuccess() {
+        logoutButton.visibility = View.GONE
+        group.visibility = View.VISIBLE
+
+    }
+
+    private fun showError(errorText: String) {
+        Toast.makeText(this, errorText, Toast.LENGTH_LONG).show()
+
+    }
+
 }
